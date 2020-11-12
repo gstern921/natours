b34c7f0556b29040b0684694bd0a2054a66d258d
Gary Stern <gcrunkster@gmail.com>
2020-11-12 17:30:03 -0500

    Updating JWT cookie settings

--- utils/sendJWTCookie.js
+++ utils/sendJWTCookie.js
@@ -2,7 +2,7 @@ module.exports = (token, req, res) => {
   const cookieOptions = {
     httpOnly: true,
     maxAge: +process.env.JWT_COOKIE_EXPIRES_IN,
-    secure: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   };
   res.cookie('jwt', token, cookieOptions);
 };
